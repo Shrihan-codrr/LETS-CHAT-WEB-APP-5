6a9c11fd8e09a2f733365eae42f098e62d1d651a
Shrihan-codrr <97533437+Shrihan-codrr@users.noreply.github.com>
2022-06-30 19:18:37 +0530

    Add files via upload

---
+++ chat_page.js
@@ -0,0 +1,72 @@
+function Logout(){
+    localStorage.removeItem("user_name");
+
+    localStorage.removeItem("room_names");
+
+    window.location  = "index.html";
+}
+
+
+//ADD YOUR FIREBASE LINKS HERE
+
+var firebaseConfig = {
+    apiKey: "AIzaSyCPoj-QNI616qgWHwxn8Zj5Ij3e3509Jfo",
+    authDomain: "lets-chat-web-app-70bae.firebaseapp.com",
+    databaseURL: "https://lets-chat-web-app-70bae-default-rtdb.firebaseio.com",
+    projectId: "lets-chat-web-app-70bae",
+    storageBucket: "lets-chat-web-app-70bae.appspot.com",
+    messagingSenderId: "73948961809",
+    appId: "1:73948961809:web:910a06a76b62d5a0ffe5b0"
+  };
+  
+  // Initialize Firebase
+// Initialize Firebase
+firebase.initializeApp(firebaseConfig);
+
+
+
+ user_name = localStorage.getItem("user_name");
+
+ room_name = localStorage.getItem("room_select");
+ 
+ document.getElementById("Welcome_user").innerHTML="Welcome " + user_name + " to #"+ room_name;
+
+ function send(){
+
+    New_message = document.getElementById("msg").value;
+
+    firebase.database().ref(room_name).push({
+          name:user_name,
+          message:New_message,
+          like:0
+    });
+
+    document.getElementById("msg").value = "";
+ }
+
+ function getData(){
+   
+  firebase.database().ref("/" + room_name).on('value',function(snapshot){
+
+    document.getElementById("output").innerHTML = "";
+    snapshot.forEach(function(childSnapshot){
+      childKey = childSnapshot.key;
+      child_data = childSnapshot.val();
+       if (childKey != "purpose"){
+        message_id = childKey;
+        message_data = child_data;
+
+
+        name_user = message_data['name'];
+        msg = message_data['message'];
+        likes = message_data['like'];  
+       }
+
+      
+    });
+  });
+
+
+
+}
+ getData();
